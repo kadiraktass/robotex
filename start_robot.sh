24ce8e43ab9f8864938eff264286a5a5d3071b24
Rob(ot) <rob.oteermmxvii@yandex.com>
2017-10-27 18:46:37 +0300

    autostart shell script backup

---
+++ start_robot.sh
@@ -0,0 +1,25 @@
+ #!/bin/bash
+
+
+echo "--- wait"
+#test for internet: 
+#but we might not have internet, but still need to run. 10+ second delay is not too long?
+
+for i in {1..20}
+do
+	httping -aqc 1 http://google.com && echo "WE HAVE CONNECTION" && break 
+	sleep 1
+	echo "loop"
+	echo -n ${i} 
+done
+
+
+echo "--- start"
+
+git pull
+python software/test_movement.py
+
+
+echo "--- finish"
+read -p "Press any key to finish" -n1 -s
+echo
