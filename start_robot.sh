b5b4636f9a15f172a8b0db04ce854e20fc43a112
Rob(ot) <rob.oteermmxvii@yandex.com>
2017-10-29 01:12:54 +0300

    proposed config.py. main.py gets autostarted, after git pull. Last commit from robot.

--- start_robot.sh
+++ start_robot.sh
@@ -5,7 +5,7 @@ echo "--- wait"
 #test for internet: 
 #but we might not have internet, but still need to run. 10+ second delay is not too long?
 
-for i in {1..20}
+for i in {1..50}
 do
 	httping -aqc 1 http://google.com && echo "WE HAVE CONNECTION" && break 
 	sleep 1
@@ -17,7 +17,8 @@ done
 echo "--- start"
 
 git pull
-python software/test_movement.py
+
+python software/main.py
 
 
 echo "--- finish"
