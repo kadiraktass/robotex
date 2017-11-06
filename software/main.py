698cc6ff236f6146db6491e65a79ee0f0cbb31d5
roboteer <rob.oteermmxvii@yandex.com>
2017-11-06 19:27:31 +0200

    safdsaf

--- software/main.py
+++ software/main.py
@@ -1,5 +1,5 @@
 
-#!/usr/bin/python2.7 
+#!/usr/bin/python2.7
 #
 #   MAIN.PY IS AUTOMATICALLY STARTED ON REBOOT
 #
@@ -9,12 +9,12 @@
 
 #import platform
 #if platform.node() == 'riiul-nuc':
-#	serialport = "/dev/ttyACM0" 
+#	serialport = "/dev/ttyACM0"
 #	cameranum = 0
 #else:
 #	serialport = "COM3"
 #	cameranum = 1
-	
+
 #gets messy. Stack it away somewhere.
 
 import cv2
@@ -44,9 +44,9 @@ greenLower = (35, 126, 82)
 greenUpper = (77, 218, 255)
 
 camera = cv2.VideoCapture(0)
-port = "/dev/ttyACM0" 
+port = "/dev/ttyACM0"
 
-#baud = 9600 
+#baud = 9600
 #ser = serial.Serial(port, baud, timeout=1)
 
 lastcommand = ""
@@ -55,48 +55,53 @@ i=0
 communication.send_soon("init")
 communication.set_thrower(60)
 
-while 1:
+try:
+	while 1:
 
-    (grabbed, frame) = camera.read()
-    
-    ball_x1, ball_y1, ball_radius1, ball_center1, ball_mask = detect_object.track(camera, greenLower, greenUpper)
-    basket_radius = -1
-    basket_x = -1
-    
-    #if ball_radius1 > 10:
-    #    cv2.circle(frame, ball_center1, 5, (0, 0, 255), -1)
-    
-    cv2.imshow("mask", ball_mask)
-    #cmd = movement.get_command(ball_x1, ball_radius1, basket_x, basket_radius)
-    m1,m2,m3 = movement.get_command(ball_x1, ball_radius1, basket_x, basket_radius)
-    #if (cmd <> lastcommand):
-        
-        #ser.write(cmd + '\r\n')
-    communication.set_motors(m1,m2,m3)
-    #    lastcommand = cmd
-        #print(cmd)
-    print(ball_radius1)
-        #print(lastcommand)
-    #else:
-       # i=i+1
-        #print(i)
-        
-#    if i==120:
-        #ser.write(cmd + '\r\n')
-   #     communication.set_motors(temp1,temp3,temp2)
-      #  i=0
-        #print(cmd)
-    
-    cv2.putText(frame, "dx: {}, dy: {}, radius: {}".format(int(ball_x1), int(ball_y1), int(ball_radius1)),
-                    (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.35, (0, 0, 255), 1)
-    cv2.imshow("Frame", frame)        
+	    (grabbed, frame) = camera.read()
 
-    communication.update_comms()
+	    ball_x1, ball_y1, ball_radius1, ball_center1, ball_mask = detect_object.track(camera, greenLower, greenUpper)
+	    basket_radius = -1
+	    basket_x = -1
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+	    #if ball_radius1 > 10:
+	    #    cv2.circle(frame, ball_center1, 5, (0, 0, 255), -1)
+
+	    cv2.imshow("mask", ball_mask)
+	    #cmd = movement.get_command(ball_x1, ball_radius1, basket_x, basket_radius)
+	    m1,m2,m3 = movement.get_command(ball_x1, ball_radius1, basket_x, basket_radius)
+	    #if (cmd <> lastcommand):
+
+	        #ser.write(cmd + '\r\n')
+	    communication.set_motors(m1,m2,m3)
+	    #    lastcommand = cmd
+	        #print(cmd)
+	    print(ball_radius1)
+	        #print(lastcommand)
+	    #else:
+	       # i=i+1
+	        #print(i)
+
+	#    if i==120:
+	        #ser.write(cmd + '\r\n')
+	   #     communication.set_motors(temp1,temp3,temp2)
+	      #  i=0
+	        #print(cmd)
+
+	    cv2.putText(frame, "dx: {}, dy: {}, radius: {}".format(int(ball_x1), int(ball_y1), int(ball_radius1)),
+	                    (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+	                    0.35, (0, 0, 255), 1)
+	    cv2.imshow("Frame", frame)
+
+	    communication.update_comms()
+
+	    key = cv2.waitKey(1) & 0xFF
+	    if key == ord("q"):
+	        break
+
+except KeyboardInterrupt:
+	communication.set_motors(0,0,0)
+	communication.set_thrower(0)
 
 camera.release()
 cv2.destroyAllWindows()
