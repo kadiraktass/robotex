8c99f562acf4dc0fc6285f3b8e7060f1cdd54167
kadiraktass <kadir-aktas@outlook.com>
2017-11-13 17:19:36 +0200

    movement debug

--- software/movement.py
+++ software/movement.py
@@ -23,41 +23,44 @@ def set_thrower_speed(basket_radius):
     
 def get_command(ball_x, ball_radius, basket_x, basket_diag):
 
-    wheelLinearVelocity1, wheelLinearVelocity2, wheelLinearVelocity3, thrower_speed = calculate_speed(ball_x, ball_radius, basket_x, basket_diag)
+    wheelLinearVelocity1, wheelLinearVelocity2, wheelLinearVelocity3, thrower_speed, rotate = calculate_speed(ball_x, ball_radius, basket_x, basket_diag,rotate)
     
     wheelSpeedToMainboardUnits = 91         #TODO: Determine the exact value
     
     wheelAngularSpeedMainboardUnits1 = wheelLinearVelocity1 * wheelSpeedToMainboardUnits
     wheelAngularSpeedMainboardUnits2 = wheelLinearVelocity2 * wheelSpeedToMainboardUnits
     wheelAngularSpeedMainboardUnits3 = wheelLinearVelocity3 * wheelSpeedToMainboardUnits
-    
-    temp1 = str(abs(int(wheelAngularSpeedMainboardUnits1)))
-    while len(temp1) < 3:
-        temp1 = '0' + temp1
-    
-    temp2 = str(abs(int(wheelAngularSpeedMainboardUnits2)))
-    while len(temp2) < 3:
-        temp2 = '0' + temp2
-    
-    temp3 = str(abs(int(wheelAngularSpeedMainboardUnits3)))
-    while len(temp3) < 3:
-        temp3 = '0' + temp3
-        
-    if wheelAngularSpeedMainboardUnits1 < 0:
-        temp1 = '-' + temp1
-    else:
-        temp1 = '0' + temp1
-    
-    if wheelAngularSpeedMainboardUnits2 < 0:
-        temp2 = '-' + temp2
-    else:
-        temp2 = '0' + temp2
+    if rotate == 0:
+        temp1 = str(abs(int(wheelAngularSpeedMainboardUnits1)))
+        while len(temp1) < 3:
+            temp1 = '0' + temp1
 
-    if  wheelAngularSpeedMainboardUnits3 < 0:
-        temp3 = '-' + temp3
-    else:
-        temp3 = '0' + temp3
-    
+        temp2 = str(abs(int(wheelAngularSpeedMainboardUnits2)))
+        while len(temp2) < 3:
+            temp2 = '0' + temp2
+
+        temp3 = str(abs(int(wheelAngularSpeedMainboardUnits3)))
+        while len(temp3) < 3:
+            temp3 = '0' + temp3
+            
+        if wheelAngularSpeedMainboardUnits1 < 0:
+            temp1 = '-' + temp1
+        else:
+            temp1 = '0' + temp1
+
+        if wheelAngularSpeedMainboardUnits2 < 0:
+            temp2 = '-' + temp2
+        else:
+            temp2 = '0' + temp2
+
+        if  wheelAngularSpeedMainboardUnits3 < 0:
+            temp3 = '-' + temp3
+        else:
+            temp3 = '0' + temp3
+    else
+        temp1 = 0
+        temp2 = 0
+        temp3 = 6
     #cmd = 'sm'+ ':' + temp1 + ':'+ temp3+ ':'+ temp2        #add thrower speed thrower_speed
     
     return temp1, temp3, temp2
@@ -169,8 +172,8 @@ def find_directions(ball_x, ball_radius, basket_x, basket_diag):
             movement_angle = 0
             thrower_speed = 0
             state = 1
-        
-    return movement_angle*3.14/180, angular_v*3.14/180, desired_speed, thrower_speed
+    rotate = 1    
+    return movement_angle*3.14/180, angular_v*3.14/180, desired_speed, thrower_speed, rotate
     
     
 
