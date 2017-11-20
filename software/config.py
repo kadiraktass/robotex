dcb34004381349aa68baa59d28e2f9c2d904db66
roboteer <rob.oteermmxvii@yandex.com>
2017-11-20 17:21:36 +0200

    csd

--- software/config.py
+++ software/config.py
@@ -14,8 +14,8 @@ ARUCOWIDTH = 138 #real-life width of aruco marker, black border edge-to-edge
 ARUCODISTANCE = 250 #distance between two markers, center-to-center
 #basket is defined by two aruco markers and optionally blob of color (HSV) (not implemented).
 #RAL code did not give meaningful detection, therefore manual calibration is required
-#BASKET = (10, 11,   (122, 77, 58), (157, 139, 201)) #Magenta RAL4010
-BASKET = (21, 22,   (63, 202, 45) ,(107, 255, 160)) #Blue RAL5015
+#BASKET = (10, 11,   (0, 0, 0), (12, 244, 114)) #Magenta RAL4010
+BASKET = (21, 22,   (63, 150, 0),  (128, 255, 150)) #Blue RAL5015
 
 
 
