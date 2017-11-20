fcd3f14c05ee97db48e86d43acb808615e910667
Rob(ot) <rob.oteermmxvii@yandex.com>
2017-11-20 22:38:56 +0200

    logic reworked

--- software/movement.py
+++ software/movement.py
@@ -3,17 +3,25 @@
 import numpy as np
 import time
 import math
-<<<<<<< HEAD
+from enum import Enum
 #from detect_aruco import calculate_speed as set_thrower_speed
-=======
+
 from detect_aruco import calculate_speed as set_thrower_speed
 from communication import millis
->>>>>>> 76764540531a4ecc0c4e0987a81abe0046185b52
+
 
 # (0,0) -----------------------  (600,0)
 #   |                               |
 #   |                               |
 # (0,450) -----------------------(600,450)
+
+class  State(Enum):
+    FIND_BALL = 1
+    DRIVE_TO_BALL = 2
+    ROTATE_AROUND_BALL = 3
+    GRAB_BALL = 4
+    
+activeState = State.FIND_BALL
 
 i= 0
 j= 0
@@ -24,16 +32,18 @@ stop_rotate = 0
 rotate_speed = 0
 last_basket_x = -1
 
-<<<<<<< HEAD
+grabBallStartTime = time.time()
+
+
 wheelSpeedToMainboardUnits = 18.75 * 64 / (2 * math.pi * 0.035 * 60)
 wheelDistanceFromCenter= 0.126   #meter
-wheelAngle1=120 * math.pi / 180                   #rad
-wheelAngle2=240 * math.pi / 180                    #rad
-wheelAngle3=0                    #rad
+wheelAngle1 = -60 * math.pi / 180                   #rad
+wheelAngle2 = 60 * math.pi / 180                    #rad
+wheelAngle3= 180 * math.pi / 180                   #rad
 wheelAngles = [wheelAngle1, wheelAngle2, wheelAngle3]
-=======
+
 last_throw = millis()
->>>>>>> 76764540531a4ecc0c4e0987a81abe0046185b52
+
 
 def get_rotate_speed(last_basket_x, ball_x):
     global rotate_speed
@@ -94,13 +104,12 @@ def calculate_speeds_from_angle(robotSpeed, robotDirectionAngle, robotAngularVel
     
 def calculate_wheelLinerVel(ball_x, ball_y, ball_radius, basket_x, basket_diag):
 
-    xSpeed,ySpeed, robotSpeed, desired_speed, thrower_speed,rotate,rotate_speed = find_directions(ball_x, ball_radius, basket_x, basket_diag)
+    xSpeed,ySpeed, rotSpeed, movement_angle, angular_v, thrower_speed,rotate,rotate_speed = find_directions(ball_x,ball_y,  ball_radius, basket_x, basket_diag)
 
-    angular_v = rotSpeed
     robotDirectionAngle = math.atan2(xSpeed, ySpeed)         #rad
     robotAngularVelocity = angular_v
 
-    wheelLinearVelocity1, wheelLinearVelocity2, wheelLinearVelocity3 = calculate_speeds(xSpeed, ySpeed, rotSpeed)
+    wheelLinearVelocity1, wheelLinearVelocity2, wheelLinearVelocity3 = calculate_speeds_from_xy(xSpeed, ySpeed, rotSpeed)
 
     return wheelLinearVelocity1, wheelLinearVelocity2, wheelLinearVelocity3, thrower_speed,rotate,rotate_speed
 
@@ -110,17 +119,72 @@ def get_angular_speed(ball_x):
         angular_v = 5
     return angular_v
     
-def find_directions(ball_x, ball_radius, basket_x, basket_dist):
+def find_directions(ball_x, ball_y, ball_radius, basket_x, basket_dist):
+    global activeState
+    global grabBallStartTime
+    
     movement_angle = 0
     angular_v = 0
-    desired_speed = 0
     thrower_speed = 0
     rotate_r = 0
     rotate_speed = 0
+    xSpeed = 0
+    ySpeed = 0
+    rotSpeed = 0
     
-    angular_v = abs(ball_x - 300) * 10 / 300
+    basketInCenter = abs(basket_x  - 300) <= 20
+    seesBall = ball_x != -1
+    print("basketInCenter: ", basketInCenter)
+    print("basket_x: ", basket_x)
+    print("basket_dist: ", basket_dist)
+    print("set_thrower_speed(basket_dist): ", set_thrower_speed(basket_dist))
     
-    return movement_angle * 3.14 / 180, angular_v, desired_speed, thrower_speed, rotate_r, rotate_speed
+    if activeState  != State.GRAB_BALL:
+        if (ball_x == -1):
+            activeState = State.FIND_BALL
+        else:
+            activeState = State.DRIVE_TO_BALL
+            
+        if ball_x != -1 and basketInCenter:
+            activeState = State.DRIVE_TO_BALL
+            
+        if not basketInCenter and ball_y >= 380:
+            activeState = State.ROTATE_AROUND_BALL
+            
+        if basketInCenter and ball_y >= 420:
+            activeState = State.GRAB_BALL
+            grabBallStartTime = time.time()
+            
+    else:
+        if time.time() - grabBallStartTime > 5:
+            activeState = State.FIND_BALL
+            
+    print(activeState)
+            
+    if (activeState == State.FIND_BALL):
+        rotSpeed = 1
+        
+    elif (activeState == State.DRIVE_TO_BALL):
+        rotSpeed = (ball_x - 300) * 1 / 300
+        ySpeed = 0.5 * abs(430 - ball_y) / 430
+        
+    elif (activeState == State.ROTATE_AROUND_BALL):
+        if (basket_x == -1):
+            rotSpeed = 0.5
+            xSpeed = -0.1
+        else:
+            rotSpeed = (basket_x - 300) * 0.5 / 300
+            xSpeed = (basket_x - 300) * -0.1 / 300
+        
+    elif (activeState == State.GRAB_BALL):
+            ySpeed = 0.05
+            rotSpeed = (basket_x - 300) * 0.2 / 300
+            thrower_speed = set_thrower_speed(basket_dist) #*3/2
+            #thrower_speed = 1000
+    
+    #thrower_speed = 1500
+    
+    return xSpeed, ySpeed, rotSpeed, movement_angle * 3.14 / 180, angular_v, thrower_speed, rotate_r, rotate_speed
 
 def find_directions_old(ball_x, ball_radius, basket_x, basket_dist):
     global last_basket_x
@@ -137,7 +201,7 @@ def find_directions_old(ball_x, ball_radius, basket_x, basket_dist):
     global j
     global i
     global rotate_r
-<<<<<<< HEAD
+
     
     if ball_x>315:               #TODO: Determine the exact value
         #turn right until x 290 310
@@ -146,7 +210,7 @@ def find_directions_old(ball_x, ball_radius, basket_x, basket_dist):
         #angular_v = get_angular_speed(ball_x)	#30        #TODO: Determine the exact value
         #thrower_speed = 0
         state = 1
-=======
+
     global last_throw
 
     if state == 1  and ball_x>315:               #TODO: Determine the exact value
@@ -156,7 +220,7 @@ def find_directions_old(ball_x, ball_radius, basket_x, basket_dist):
         angular_v = get_angular_speed(ball_x)	#30        #TODO: Determine the exact value
         thrower_speed = 0
         #state = 1
->>>>>>> 76764540531a4ecc0c4e0987a81abe0046185b52
+
         rotate_r = 0
 
     elif state == 1 and 285>ball_x>0:                   #TODO: Determine the exact value
