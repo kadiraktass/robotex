67f29892cb2a0c34684cf3bdcb91a182af2efe32
roboteer <rob.oteermmxvii@yandex.com>
2017-11-24 12:56:07 +0200

    sdf

--- software/main.py
+++ software/main.py
@@ -51,7 +51,8 @@ if not args.target is None:
 if not args.tambov is None:
     detect_aruco2.TAMBOV = int(args.tambov)
     print (detect_aruco2.TAMBOV)
-    return
+
+input()
 
 
 #print('PARAMS: FIELD=', FIELD_ID, ', ROBOT=', ROBOT_ID, ', BRAKES=', BRAKES_ON, ', TARGET=', TARGET_BASKET)
