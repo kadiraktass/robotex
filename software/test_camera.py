6f2cbab9f2ea260fc7dd81574fa27201de12751b
roboteer <rob.oteermmxvii@yandex.com>
2017-11-25 14:55:15 +0200

    fas

--- software/test_camera.py
+++ software/test_camera.py
@@ -3,6 +3,10 @@ import cv2
 from config import *
 
 camera = cv2.VideoCapture( cameranum )
+camera.set(13, 0.40) #hue
+camera.set(14, 0.04)
+
+
 while True:
     (grabbed, frame) = camera.read()
     cv2.imshow("Frame", frame)
