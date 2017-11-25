dbc54ced9e14c68bb53122260cb2c6d6a36e1346
kadiraktass <kadir-aktas@outlook.com>
2017-11-25 18:07:23 +0200

    asd

--- software/movement.py
+++ software/movement.py
@@ -137,7 +137,7 @@ def find_directions(ball_x, ball_y, ball_radius, basket_x, basket_dist,orangeAre
         rotSpeed = -2
         print("activeState = ", activeState)
         print("findBallStartTime = ", findBallStartTime)
-        if time.time() - findBallStartTime > 2:
+        if time.time() - findBallStartTime > 1:
             if(orangeArea> 150000):
                 print("find ball directional move = ")
                 ySpeed = 1    
