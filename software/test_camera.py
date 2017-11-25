b35ce920bffd2b3bafa70eb5dac3e29302b01e5a
roboteer <rob.oteermmxvii@yandex.com>
2017-11-25 15:00:15 +0200

    fsf

--- software/test_camera.py
+++ software/test_camera.py
@@ -3,13 +3,38 @@ import cv2
 from config import *
 
 camera = cv2.VideoCapture( cameranum )
-camera.set(13, 0.40) #hue
-camera.set(14, 0.04)
+
+hue = 0.4
+exp = 0.04
 
 
 while True:
+    camera.set(13, hue)
+    camera.set(14, exp)
+
+
     (grabbed, frame) = camera.read()
     cv2.imshow("Frame", frame)
+
+    cv2.putText(frame, "HUE: {}".format(hue),
+                        (50, 80), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (255, 255, 255), 1)
+
+    cv2.putText(frame, "EXP: {}".format(exp),
+                        (50, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (255, 255, 255), 1)
+
+
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
+    elif key == ord("e"):
+        hue += 0.01
+
+    elif key == ord("d"):
+        hue -= 0.01
+
+    elif key == ord("r"):
+        exp += 0.01
+    elif key == ord("f"):
+        exp -= 0.01
