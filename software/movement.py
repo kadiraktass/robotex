e87acdb87655440f904a3480bf82b21f38c8e1fc
kadiraktass <kadir-aktas@outlook.com>
2017-11-25 17:36:36 +0200

    asd

--- software/movement.py
+++ software/movement.py
@@ -137,10 +137,12 @@ def find_directions(ball_x, ball_y, ball_radius, basket_x, basket_dist,orangeAre
             activeState = State.FIND_BALL
 
     if (activeState == State.FIND_BALL):
-        rotSpeed = -2.5
+        rotSpeed = -2
+        print("activeState = ", activeState)
         print("findBallStartTime = ", findBallStartTime)
         if time.time() - findBallStartTime > 1:
-            ySpeed = 0.4
+            print("find ball directional move = ")
+            ySpeed = 1
             rotSpeed = -0.4    
     elif (activeState == State.DRIVE_TO_BALL):
         rotSpeed = 1.5*(ball_x - 300) * 1 / 300
